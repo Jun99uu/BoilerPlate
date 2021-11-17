5447932b92141c9819b239c1e0f30075c960bf5e
jun99uu <igun0423@naver.com>
2021-11-17 23:47:31 +0900

    boiler plate 7

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const {User} = require('./models/User');
+
+//bodyParser가 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해주는 것
+// applicatinx/x-www-form-ulencoded로 분석해서 가져올 수 있게 해줌
+app.use(express.urlencoded({extended: true}));
+//application/json으로 분석해서 가져올 수 있게 해줌
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://rhon:wnsrb990614@boilerplate.v0nvl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World! 하이욤ㅇㅁㅇㅎㅇㅎㅇ')
+
+app.post('/register', (req, res) =>{
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+//몽고db에서 오는 메소드 저장
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({ //성공했음을 의미
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-const mongooser = require('mongoose');
+const mongoose = require('mongoose');
 
 
-const userSchema = mongoose.schema({
+const userSchema = mongoose.Schema({
   name : {
     type:String,
     maxlength: 50
   },
   email: {
     type: String,
-    trip: true, //스페이스 없애주는역할
+    trim: true, //스페이스 없애주는역할
     unique: 1//같은 이름 사용하지 못함
   },
   password: {
@@ -21,11 +21,11 @@ const userSchema = mongoose.schema({
   },
   role: { //관리자 등의 역할
     type: Number,
-    degault: 0
+    default: 0
   },
   image: String,
-  token : { //유효성 판단하는 토큰
-    type:String
+  token: { //유효성 판단하는 토큰
+    type: String
   },
   tokenExp: { //토큰의 유효기간
     type: Number
@@ -35,4 +35,4 @@ const userSchema = mongoose.schema({
 //스키마를 모델로 감싸줌
 const User = mongoose.model('User', userSchema)
 
-module.exposts = {User}
+module.exports = {User}
